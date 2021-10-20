0577cdec69acd3a305df3535db51021c1659443d
CrAyZiK <41376435+CrAyZiK@users.noreply.github.com>
2021-10-20 12:06:16 +0300

    .

--- src/main/java/ru/unidubna/javaweb2/controllers/AddController.java
+++ src/main/java/ru/unidubna/javaweb2/controllers/AddController.java
@@ -1,8 +1,6 @@
 package ru.unidubna.javaweb2.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.unidubna.javaweb2.model.User;
 import ru.unidubna.javaweb2.model.UserService;
 
@@ -16,19 +14,19 @@ import java.io.IOException;
 public class AddController {
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public /*String*/void addControllerGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/add");
-        requestDispatcher.forward(req, resp);
-//        return "jsp/add";
+    public String addControllerGet(/*HttpServletRequest req, HttpServletResponse resp*/) /*throws ServletException, IOException*/ {
+        /*RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/add");
+        requestDispatcher.forward(req, resp);*/
+        return "jsp/add";
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public /*String*/ void addControllerPost(HttpServletRequest req){
+    public String addControllerPost(HttpServletRequest req){
         String name = req.getParameter("name");
         int age = Integer.parseInt(req.getParameter("age"));
         User user = new User(name, age);
         UserService userService = new UserService();
         userService.saveUser(user);
-//        return "jsp/add";
+        return "jsp/add";
     }
 }
--- src/main/java/ru/unidubna/javaweb2/controllers/ListController.java
+++ src/main/java/ru/unidubna/javaweb2/controllers/ListController.java
@@ -1,6 +1,7 @@
 package ru.unidubna.javaweb2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.unidubna.javaweb2.model.User;
@@ -17,13 +18,13 @@ import java.util.stream.Collectors;
 @Controller
 public class ListController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public /*String*/ void listController(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public String listController(HttpServletRequest req/*, HttpServletResponse resp*/) /*throws ServletException, IOException*/ {
         UserService userService = new UserService();
         List<User> users = userService.findAllUsers();
         List<String> names =  users.stream().map(User::getName).collect(Collectors.toList());
         req.setAttribute("userNames", names);
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/list");
-        requestDispatcher.forward(req, resp);
-//        return "forward:jsp/list";
+        //RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/list");
+        //requestDispatcher.forward(req, resp);
+        return "jsp/list";
     }
 }
